1a4fbc8f999a228481a611852a8c9cd94b5df58f
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-10-22 11:41:03 +0300

    Add Links into Client page

--- app/client/page.js
+++ app/client/page.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function Client() {
 	return (
-		<h1>Client Page</h1>
+		<div>
+			<h1>Client Page</h1>
+			<Link href='/'>Main Page</Link><br />
+			<Link href='/client/12345'>Client 12345</Link>
+		</div>
+		
 	);
 }
