1145b8641b2fb4e15f7feb44a733acb0f237c3b6
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-10-22 21:31:19 +0300

    Add receiving and outputting data from URL address

--- app/page.js
+++ app/page.js
@@ -1,5 +1,25 @@
-export default function Home() {
+import Link from "next/link";
+
+async function fetchData () {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+	const result = await res.json();
+	return result;
+}
+
+export default async function Home() {
+	const posts = await fetchData();
+
 	return (
-		<h1>Main Page</h1>
+		<div>
+			<h1>Main Page</h1>
+			{posts.map(el => (
+				<div key={el.id} className="post">
+					<h2>{el.title}</h2>
+					<p>{el.body}</p>
+					<Link href={`/post/` + el.id}>More details</Link>
+				</div>
+			))}
+		</div>
+		
 	);
 }
