a8cbb7cedd0aa326ee816d1d1fd9022b7c1ba855
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2024-10-24 23:28:29 +0300

    Add receiving the data for the page

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,6 +1,21 @@
-const Post = ({ params }) => {
+import Link from "next/link";
+
+async function fetchData (id) {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+	const result = await res.json();
+	return result;
+}
+
+const Post = async ({ params: {id} }) => {
+	const post = await fetchData(id);
+
 	return (
-		<div>{params.id}</div>
+		<div className="post">
+			<Link href='/'>Back</Link>
+			<h2>{post.title}</h2>
+			<p>{post.body}</p>
+			<strong>Author ID: {post.userId}</strong>
+		</div>
 	)
 }
 
